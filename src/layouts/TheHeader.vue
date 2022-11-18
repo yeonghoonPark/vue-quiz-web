36932975895fa4afae8a0d0902723d67c7935a99
yeonghoon Park <huklmnj2@naver.com>
2022-11-18 18:16:44 +0900

    [fix] modify layouts
    - TheHeader.vue
    - TheView.vue

---
+++ src/layouts/TheHeader.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <h1>temp header</h1>
+</template>
---
+++ src/layouts/TheView.vue
@@ -0,0 +1,7 @@
+<template>
+  <main>
+    <div class="container py-4">
+      <RouterView />
+    </div>
+  </main>
+</template>
