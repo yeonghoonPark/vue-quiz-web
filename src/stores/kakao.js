75accddb961771c7170efd67d7ee9b81acb61aeb
yeonghoon Park <huklmnj2@naver.com>
2022-12-03 22:13:55 +0900

    [LoginView] kakao login methods move to kakao.js

--- src/stores/kakao.js
+++ src/stores/kakao.js
@@ -3,7 +3,10 @@ import { defineStore } from "pinia";
 export const useKakaoStore = defineStore("kakao", {
   // ref
   state: () => ({
-    test: 0,
+    access_token: null,
+    account_email: null,
+    gender: null,
+    profile_nickname: null,
   }),
 
   // computed
@@ -11,8 +14,66 @@ export const useKakaoStore = defineStore("kakao", {
 
   // method
   actions: {
-    testFunction: function () {
-      console.log(this.test, "testFunction by pinia");
+    getUserInfo: function () {
+      console.log("[getUserInfo]");
+      Kakao.API.request({
+        url: "/v2/user/me",
+        success: function (response) {
+          console.log(response, "response");
+        },
+        fail: function (error) {
+          alert(
+            `카카로 로그인에 실패했습니다. 관리자에게 문의하세요.
+            ${JSON.stringify(error)}`,
+          );
+        },
+      });
+    },
+
+    loginWithKakao: function () {
+      console.log("[loginWithKakao]");
+      Kakao.Auth.loginForm({
+        scope: "account_email gender profile_nickname",
+        success: function (auth) {
+          console.log(auth, "@@auth");
+          console.log(auth.access_token, "@@auth.access_token");
+
+          Kakao.Auth.setAccessToken(auth.access_token);
+
+          this.access_token = auth.access_token;
+          console.log(this.access_token, "@@this.ACCESS_TOKEN");
+
+          Kakao.API.request({
+            url: "/v2/user/me",
+            success: function (response) {
+              console.log(response, "response");
+            },
+            fail: function (error) {
+              alert(
+                `카카로 로그인에 실패했습니다. 관리자에게 문의하세요.
+                ${JSON.stringify(error)}`,
+              );
+            },
+          });
+        },
+        fail: function (error) {
+          console.log(error);
+        },
+      });
+    },
+
+    logoutWithKakao: function () {
+      console.log("[logoutWithKakao]");
+
+      if (!Kakao.Auth.getAccessToken()) {
+        console.log("Not logged in.");
+        return;
+      }
+
+      Kakao.Auth.logout(function () {
+        // this.ACCESS_TOKEN = null;
+        console.log(`logout ok\nAccessToken = ${Kakao.Auth.getAccessToken()}`);
+      });
     },
   },
 });
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,94 +1,77 @@
 <script setup>
-import { ref } from "vue";
+import { onMounted, ref, watch } from "vue";
 import { useKakaoStore } from "@/stores/kakao";
 import { storeToRefs } from "pinia";
 import BaseButton from "@/components/base/BaseButton.vue";
 
 // 토큰 state, 차후 pinia state로 변경
-const ACCESS_TOKEN = ref(null);
+// const ACCESS_TOKEN = ref(null);
 
 const store = useKakaoStore();
 
 // store's state
-const { test } = storeToRefs(store);
-console.log(test.value);
+const { access_token } = storeToRefs(store);
 
 // store's method
-const { testFunction } = store;
-testFunction();
+const { loginWithKakao, logoutWithKakao } = store;
 
-// const loginWithKakao = () => {
-//   console.log("로그인");
-//   Kakao.Auth.login({
-//     scope: "profile_nickname, account_email, gender",
-//     success: function (authObj) {
-//       console.log(authObj, "authObj");
-//       Kakao.Auth.setAccessToken(authObj.access_token);
-//       Kakao.API.request({
-//         url: "/v2/user/me",
-//         success: (res) => {
-//           const kakao_account = res.kakao_account;
-//           console.log(kakao_account, "kakao_account");
-//         },
-//       });
+// const getUserInfo = () => {
+//   console.log("[getUserInfo]");
+//   Kakao.API.request({
+//     url: "/v2/user/me",
+//     success: function (response) {
+//       console.log(response, "response");
+//     },
+//     fail: function (error) {
+//       alert(
+//         `카카로 로그인에 실패했습니다. 관리자에게 문의하세요.
+//         ${JSON.stringify(error)}`,
+//       );
 //     },
 //   });
 // };
 
-const getUserInfo = () => {
-  console.log("[getUserInfo]");
-  Kakao.API.request({
-    url: "/v2/user/me",
-    success: function (response) {
-      console.log(response, "response");
-    },
-    fail: function (error) {
-      alert(
-        `카카로 로그인에 실패했습니다. 관리자에게 문의하세요. 
-        ${JSON.stringify(error)}`,
-      );
-    },
-  });
-};
+// const loginWithKakao = () => {
+//   console.log("[loginWithKakao]");
+//   Kakao.Auth.loginForm({
+//     success: function (auth) {
+//       console.log(auth);
+//       console.log(auth.access_token, "auth.access_token");
 
-const loginWithKakao = () => {
-  console.log("[loginWithKakao]");
-  Kakao.Auth.loginForm({
-    success: function (auth) {
-      console.log(auth);
-      console.log(auth.access_token, "auth.access_token");
+//       Kakao.Auth.setAccessToken(auth.access_token);
+//       console.log("setAccessToken", "setAccessToken");
 
-      Kakao.Auth.setAccessToken(auth.access_token);
-      console.log("setAccessToken", "setAccessToken");
+//       ACCESS_TOKEN.value = auth.access_token;
+//       console.log(ACCESS_TOKEN.value, "ACCESS_TOKEN.value");
 
-      ACCESS_TOKEN.value = auth.access_token;
-      console.log(ACCESS_TOKEN.value, "ACCESS_TOKEN.value");
+//       getUserInfo();
+//     },
+//     fail: function (error) {
+//       console.log(error);
+//     },
+//   });
+// };
 
-      getUserInfo();
-    },
-    fail: function (error) {
-      console.log(error);
-    },
-  });
-};
+// const logoutWithKakao = () => {
+//   console.log("[logoutWithKakao]");
+//   if (!Kakao.Auth.getAccessToken()) {
+//     console.log("Not logged in.");
+//     return;
+//   }
+//   Kakao.Auth.logout(function () {
+//     ACCESS_TOKEN.value = null;
+//     console.log(`logout ok\nAccessToken = ${Kakao.Auth.getAccessToken()}`);
+//   });
+// };
 
-const logoutWithKakao = () => {
-  console.log("[logoutWithKakao]");
-  if (!Kakao.Auth.getAccessToken()) {
-    console.log("Not logged in.");
-    return;
-  }
-  Kakao.Auth.logout(function () {
-    ACCESS_TOKEN.value = null;
-    console.log(`logout ok\nAccessToken = ${Kakao.Auth.getAccessToken()}`);
-  });
-};
+onMounted(() => {
+  console.log(access_token.value, "밸류");
+});
 </script>
 
 <template>
   <div id="LoginView">
     <BaseButton
-      v-if="!ACCESS_TOKEN"
       class="btn btn-warning btn-sm"
       :message="'Kakao Login'"
       @click="loginWithKakao"
