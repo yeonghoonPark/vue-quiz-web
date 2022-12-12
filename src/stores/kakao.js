2927c66e22df2aab58c5fc071ac13d0790a17ef0
yeonghoon Park <huklmnj2@naver.com>
2022-12-12 22:05:46 +0900

    [kakao.js] add a alert

--- src/stores/kakao.js
+++ src/stores/kakao.js
@@ -7,9 +7,9 @@ export const useKakaoStore = defineStore("kakao", () => {
   const profile_nickname = ref(null);
 
   const resetUserInfo = () => {
-    (access_token.value = null),
-      (account_email.value = null),
-      (profile_nickname.value = null);
+    access_token.value = null;
+    account_email.value = null;
+    profile_nickname.value = null;
   };
 
   const getUserInfo = () => {
@@ -25,7 +25,8 @@ export const useKakaoStore = defineStore("kakao", () => {
         profile_nickname.value = response.properties?.nickname;
         console.log(response.properties.nickname, "@@닉네임");
 
-        localStorage.setItem("account_email", `${account_email}`);
+        // 임시 로그인 alert
+        alert(`${profile_nickname.value}님 로그인에 성공하였습니다.`);
       },
       fail: function (error) {
         alert(
@@ -49,8 +50,6 @@ export const useKakaoStore = defineStore("kakao", () => {
         console.log("setAccessToken", "setAccessToken");
 
         access_token.value = auth.access_token;
-        // ACCESS_TOKEN.value = auth.access_token;
-        // console.log(ACCESS_TOKEN.value, "ACCESS_TOKEN.value");
 
         getUserInfo();
       },
@@ -71,6 +70,9 @@ export const useKakaoStore = defineStore("kakao", () => {
     Kakao.Auth.logout(function () {
       resetUserInfo();
       console.log(`logout ok\nAccessToken = ${Kakao.Auth.getAccessToken()}`);
+
+      // 임시 로그아웃 alert
+      alert("로그아웃 되었습니다.");
     });
   };
 
@@ -81,81 +83,4 @@ export const useKakaoStore = defineStore("kakao", () => {
     loginWithKakao,
     logoutWithKakao,
   };
-
-  // /* ref */
-  // state: () => ({
-  //   access_token: null,
-  //   account_email: null,
-  //   gender: null,
-  //   profile_nickname: null,
-  // }),
-
-  // /* computed */
-  // getters: {},
-
-  // /* method */
-  // actions: {
-  //   getUserInfo: function () {
-  //     console.log("[getUserInfo]");
-  //     Kakao.API.request({
-  //       url: "/v2/user/me",
-  //       success: function (response) {
-  //         console.log(response, "response");
-  //       },
-  //       fail: function (error) {
-  //         alert(
-  //           `카카로 로그인에 실패했습니다. 관리자에게 문의하세요.
-  //           ${JSON.stringify(error)}`,
-  //         );
-  //       },
-  //     });
-  //   },
-
-  //   loginWithKakao: function () {
-  //     console.log("[loginWithKakao]");
-  //     Kakao.Auth.loginForm({
-  //       scope: "account_email gender profile_nickname",
-  //       success: function (auth) {
-  //         console.log(auth, "@@auth");
-  //         console.log(auth.access_token, "@@auth.access_token");
-
-  //         Kakao.Auth.setAccessToken(auth.access_token);
-
-  //         this.access_token = auth.access_token;
-  //         console.log(this.access_token, "@@this.ACCESS_TOKEN");
-
-  //         Kakao.API.request({
-  //           url: "/v2/user/me",
-  //           success: function (response) {
-  //             console.log(response, "response");
-  //             console.log(access_token.value, "디스");
-  //           },
-  //           fail: function (error) {
-  //             alert(
-  //               `카카로 로그인에 실패했습니다. 관리자에게 문의하세요.
-  //               ${JSON.stringify(error)}`,
-  //             );
-  //           },
-  //         });
-  //       },
-  //       fail: function (error) {
-  //         console.log(error);
-  //       },
-  //     });
-  //   },
-
-  //   logoutWithKakao: function () {
-  //     console.log("[logoutWithKakao]");
-
-  //     if (!Kakao.Auth.getAccessToken()) {
-  //       console.log("Not logged in.");
-  //       return;
-  //     }
-
-  //     Kakao.Auth.logout(function () {
-  //       // this.ACCESS_TOKEN = null;
-  //       console.log(`logout ok\nAccessToken = ${Kakao.Auth.getAccessToken()}`);
-  //     });
-  //   },
-  // },
 });
