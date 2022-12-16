3d1cd8fbc53667791a011365a9aff359b0a709a7
yeonghoon Park <huklmnj2@naver.com>
2022-12-16 18:03:17 +0900

    [QuizView] make a timer

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -2,12 +2,71 @@
 import BaseButton from "@/components/base/BaseButton.vue";
 import { ref } from "vue";
 
-const count = ref(0);
+let stTime = 0;
+let timerStart;
+
+let min;
+let sec;
+let milisec;
+
+const addZero = (number) => {
+  return number < 10 ? "0" + number : "" + number;
+};
+
+const testStart = () => {
+  console.log("[testStart]");
+
+  if (!stTime) {
+    stTime = Date.now();
+    console.log(stTime);
+  }
+
+  timerStart = setInterval(function () {
+    let nowTime = new Date(Date.now() - stTime);
+
+    min = addZero(nowTime.getMinutes());
+    sec = addZero(nowTime.getSeconds());
+    milisec = addZero(Math.floor(nowTime.getMilliseconds() / 10));
+
+    document.getElementById("postTestMin").innerText = min;
+    document.getElementById("postTestSec").innerText = sec;
+    document.getElementById("postTestMilisec").innerText = milisec;
+  }, 1);
+};
+
+const testStop = () => {
+  console.log("[testStop]");
+  clearInterval(timerStart);
+};
+
+const testReset = () => {
+  console.log("[testReset]");
+  stTime = 0;
+  min = 0;
+  sec = 0;
+  milisec = 0;
+  document.getElementById("postTestMin").innerText = "00";
+  document.getElementById("postTestSec").innerText = "00";
+  document.getElementById("postTestMilisec").innerText = "00";
+};
 </script>
 
 <template>
   <div id="QuizView">
     <BaseButton class="btn-primary btn-lg" :message="'Quiz'" @click="" />
-    <h1>{{ count }}</h1>
+
+    <hr />
+    <span id="postTestMin">00</span>
+    <span>:</span>
+    <span id="postTestSec">00</span>
+    <span>.</span>
+    <span id="postTestMilisec">00</span>
+    <hr />
+
+    <button class="btn btn-primary btn-sm me-2" @click="testStart">
+      Start
+    </button>
+    <button class="btn btn-primary btn-sm me-2" @click="testStop">Stop</button>
+    <button class="btn btn-primary btn-sm" @click="testReset">Reset</button>
   </div>
 </template>
