2dad912190b7a5a690121cd561c76a73d3d7bc93
yeonghoon Park <huklmnj2@naver.com>
2022-12-29 20:46:38 +0900

    [BaseAlert] create BaseAlert.vue

---
+++ src/components/base/BaseAlert.vue
@@ -0,0 +1,45 @@
+<script setup>
+const props = defineProps({
+  message: {
+    type: [String, Number],
+    default: "this is default alert message",
+  },
+  isShow: {
+    type: Boolean,
+    default: false,
+  },
+  classType: {
+    type: String,
+    default: "alert-primary",
+  },
+});
+</script>
+
+<template>
+  <div class="position-fixed top-50 start-50 translate-middle">
+    <Transition name="slide">
+      <div v-if="isShow" role="alert" class="alert" :class="classType">
+        {{ message }}
+      </div>
+    </Transition>
+  </div>
+</template>
+
+<style scoped>
+.slide-enter-from,
+.slide-leave-to {
+  transform: translate(-50%, -50%);
+  opacity: 0;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.1s ease-out;
+}
+
+.slide-enter-to,
+.slide-leave-from {
+  transform: translateY(-50%, -50%);
+  opacity: 1;
+}
+</style>
