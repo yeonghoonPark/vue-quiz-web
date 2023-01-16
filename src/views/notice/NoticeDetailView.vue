892551e00e18ad221f93f35fd573241dadda2e38
yeonghoon Park <huklmnj2@naver.com>
2023-01-16 23:22:45 +0900

    [NoticeDetailVeiw] css edit

--- src/views/notice/NoticeDetailView.vue
+++ src/views/notice/NoticeDetailView.vue
@@ -1,9 +1,16 @@
 <script setup>
+import BaseButton from "@/components/base/BaseButton.vue";
 import { onMounted, ref } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import notice from "@/data/notice";
+import { useKakaoStore } from "@/stores/kakao.js";
+
+const kakao = useKakaoStore();
+
+const { access_token, account_email, profile_nickname } = kakao;
 
 const route = useRoute();
+const router = useRouter();
 const selectedItem = ref(null);
 console.log(selectedItem, "셀렉티드");
 
@@ -17,16 +24,87 @@ notice.forEach((item) => {
   }
 });
 
+const goNoticeView = () => {
+  console.log("[goNoticeView]");
+  router.push({ name: "NoticeView" });
+};
+
 onMounted(() => {
   console.log("[onMounted]");
 });
 </script>
 
 <template>
-  <div id="NoticeDetailView" class="user-select-none">
-    <h1>게시판 글세부목록</h1>
-    <p>{{ selectedItem.title }}</p>
-    <p>{{ selectedItem.content }}</p>
-    <p>{{ selectedItem.createdDate }}</p>
+  <div id="NoticeDetailView" class="py-4">
+    <!-- title -->
+    <h1 class="text-center mb-5">글</h1>
+
+    <!-- main -->
+    <div class="container mb-4">
+      <div class="row align-items-center py-2">
+        <div class="col-3 text-center">
+          <span>작성자</span>
+        </div>
+        <div class="col-9">
+          <span>{{ profile_nickname }}</span>
+        </div>
+      </div>
+
+      <hr />
+
+      <div class="row align-items-center py-2">
+        <div class="col-3 text-center">
+          <span>글 분류</span>
+        </div>
+        <div class="col-9">
+          <span class="box py-1 px-3">
+            {{ selectedItem?.article }}
+          </span>
+        </div>
+      </div>
+
+      <hr />
+
+      <div class="row align-items-center py-2">
+        <div class="col-3 text-center">
+          <span>글 제목</span>
+        </div>
+        <div class="col-9">
+          <span>
+            {{ selectedItem.title }}
+          </span>
+        </div>
+      </div>
+
+      <hr />
+
+      <div class="row align-items-start py-2 mb-4">
+        <div class="col-3 text-center">
+          <span>글 내용</span>
+        </div>
+        <div class="col-9">
+          {{ selectedItem.content }}
+        </div>
+      </div>
+
+      <p></p>
+      <p></p>
+      <p>{{ selectedItem.createdDate }}</p>
+    </div>
+
+    <!-- button-group -->
+    <div class="container d-flex justify-content-end">
+      <BaseButton
+        class="btn-outline-dark me-3"
+        :message="'목록'"
+        @click="goNoticeView"
+      />
+      <BaseButton
+        class="btn-outline-success me-3"
+        :message="'수정'"
+        @click=""
+      />
+      <BaseButton class="btn-outline-danger" :message="'삭제'" @click="" />
+    </div>
   </div>
 </template>
